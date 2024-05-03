9cc1ffc66a67269468f291c9525ebbf57d37747a
Sana451 <sana451@mail.ru>
2024-05-03 19:58:07 +0300

    Add is_element_present func to BasePage, Add should_be_login_link to MainPage, Add test_gust_should_see_login_link

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser: webdriver.Chrome, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by_how, what_element):
+        try:
+            self.browser.find_element(by_how, what_element)
+            return True
+        except NoSuchElementException:
+            return False
